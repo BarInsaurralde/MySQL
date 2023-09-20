1640118ad497930ea75d8d528ad3ac2c3a248d51
Lincy Gonzalez <lincgr@gmail.com>
2023-09-20 12:00:51 +0200

    01_06 Diferencia entre VARCHAR y CHAR en MySQL

---
+++ 01_06/01_06.sql
@@ -0,0 +1,10 @@
+INSERT INTO `libreria`.`cliente`
+(`nombre`,
+`apellido`,
+`telefono`,
+`email`)
+VALUES
+('Brhadaranyakopanishadvivekachudamani Erreh Jose',
+'Rojas',
+'1478523699632587412364',
+'jose@test.com');
