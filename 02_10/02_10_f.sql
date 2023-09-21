273355656e49220067d6ef0970c9c0875b945adb
Lincy Gonzalez <lincgr@gmail.com>
2023-09-21 10:21:28 +0200

    02_10 Eliminar registros duplicados en MySQL

---
+++ 02_10/02_10_f.sql
@@ -0,0 +1,7 @@
+SET SQL_SAFE_UPDATES = 1;
+DELETE FROM empleado
+WHERE id NOT IN (
+	SELECT * 
+	FROM (SELECT MIN(e.id)
+	FROM empleado e GROUP BY e.email) empleado_mantener
+)
---
+++ 02_10/02_10_i.sql
@@ -0,0 +1,2 @@
+INSERT INTO `empleado` (nombre, apellido, email) 
+VALUES ('Juan','Rojas','juan.rojas@libreriatest.com'),('John','Smith','john.smith@libreriatest.com');
